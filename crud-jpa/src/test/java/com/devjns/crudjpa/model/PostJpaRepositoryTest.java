3f2382a20da4dc55c6e6736716608520a9329f63
Jairo Nascimento <jaironsousa@gmail.com>
2022-11-03 15:53:11 -0300

    Ajuste nomes das classes de teste

--- crud-jpa/src/test/java/com/devjns/crudjpa/model/PostJpaRepositoryTest.java
+++ crud-jpa/src/test/java/com/devjns/crudjpa/model/PostJpaRepositoryTest.java
@@ -6,82 +6,59 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @Autor Jairo Nascimento
- * @Created 03/11/2022 - 15:10
+ * @Created 03/11/2022 - 15:09
  */
-@DisplayName("Crud Repository")
-@ExtendWith(SpringExtension.class)
+@DisplayName("Jpa Repository")
 @DataJpaTest
+@ExtendWith(SpringExtension.class)
 class PostJpaRepositoryTest implements WithAssertions {
 
     @Autowired
-    private PostCrudRepository repository;
+    PostJpaRepository jpaRepository;
 
+    @DisplayName("Delete all in batch")
     @Test
-    @DisplayName("Retrieve all")
     @Sql("/data.sql")
-    void findAll() {
-        assertThat(repository.findAll()).isNotEmpty().hasSize(10);
+    void deleteAllInBatch() {
+        assertThat(jpaRepository.count()).isGreaterThan(0);
+        // Attention for the documentation
+        //Deletes the given entities in a batch which means it will create a single query.
+        // This kind of operation leaves JPAs first level cache and the database out of sync.
+        // Consider flushing the EntityManager before calling this method.
+        jpaRepository.deleteAllInBatch();
+        assertThat(jpaRepository.count()).isEqualTo(0);
     }
 
+    @DisplayName("Find all using page")
     @Test
-    @DisplayName("Retrieve all by user")
     @Sql("/data.sql")
-    void findAllByUser() {
-        var user = "luizcosta";
-        List<Post> allPostsByUser = this.repository.findAllByUsuario(user);
-        assertThat(allPostsByUser).extracting("usuario").contains(user);
+    void findAllUsingPage() {
+        var posts = jpaRepository.findAll(
+                PageRequest.of(1, 2, Sort.by(Sort.Order.desc("createdAt"))))
+                .getContent();
+
+        assertThat(posts).extracting("usuario", "content")
+                .contains(tuple("MrBeast", "Retweet, get paid"),
+                        tuple("RyanSesseman", "1 dog = many retweets"));
     }
 
+    @DisplayName("Find all using order by natural order")
     @Test
-    @DisplayName("Retrieve all by user contains word")
     @Sql("/data.sql")
-    void findAllByUserLike() {
-        var user = "t";
-        List<Post> allPostsByUser = this.repository.findAllByUsuarioContains(user);
-        assertThat(allPostsByUser).hasSize(4);
-    }
-
-    @Test
-    @DisplayName("Retrieve all posts by user and post word")
-    @Sql("/data.sql")
-    void findAllByUserAndPost() {
-        var user = "luizcosta";
-        var post = "#studying";
-        List<Post> allPostsByUser = this.repository.findAllByUsuarioAndContentContaining(user, post);
-        assertThat(allPostsByUser).hasSize(2);
-    }
-
-    @Test
-    @DisplayName("Count all posts having the content")
-    @Sql("/data.sql")
-    void countAllPostsHavingTheContent() {
-        var post = "Rock";
-        Long occurrences = this.repository.countPostContentWithContentLowerCase(post.toLowerCase());
-        assertThat(occurrences).isEqualTo(2);
-    }
-
-    @DisplayName("Retrieve all between dates")
-    @Sql("/data.sql")
-    void findAllBetweenDates() {
-        ZoneId zoneId = ZoneId.of("Europe/Madrid");
-        var localDateTime = LocalDateTime.of(2015, Month.JANUARY, 1, 0, 0, 0, 0);
-        var start = ZonedDateTime.of(localDateTime, zoneId);
-        var end = ZonedDateTime.of(localDateTime.plusYears(5), zoneId);
-
-        List<Post> allByCreatedAtBetween = this.repository.findAllByCreatedAtBetween(start, end);
-        assertThat(allByCreatedAtBetween).isNotEmpty().hasSize(6);
+    void findAllUsingOrderBy() {
+        List<Post> posts = jpaRepository.findAll(Sort.by("createdAt", "usuario"));
+        assertThat(posts).flatExtracting("createdAt", "usuario")
+                .contains("TheEllenShow", "Rubiu5", "luizcosta", "TheRock",
+                        "luizcosta", "RandallTime", "RyanSesseman", "MrBeast",
+                        "btw_twt", "10Ronaldinho");
     }
 }
